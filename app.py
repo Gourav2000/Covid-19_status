cd507a24a793eb5c5b5a837627f1a54992424e91
Gourav2000 <gouravsarkar67@gmail.com>
2020-09-17 12:00:52 +0530

    added threading

--- app.py
+++ app.py
@@ -18,19 +18,29 @@ news_dic['is_completed']=False
 def news_get():
     news_dic['news_list']=news.get_news()
     news_dic['is_completed']=True
-th=Thread(target=news_get)
-th.start()
+th_news=Thread(target=news_get)
+th_news.start()
 
-r2=requests.get('https://api.covid19india.org/data.json')
-rit=r2.json()
-India_list=rit['statewise']
-r=requests.get('https://corona.lmao.ninja/v2/countries')
-World_list=r.json()
-w_total=requests.get('https://disease.sh/v3/covid-19/all?yesterday=true&twoDaysAgo=true&allowNull=true').json()
-for i in World_list:
-    if i['country']=='India':
-        i_total=i
-        break
+detail_dic= dict()
+detail_dic['India_list']=list()
+detail_dic['World_list']=list()
+i_total=dict()
+w_total=dict()
+
+def getDetails():
+    r2=requests.get('https://api.covid19india.org/data.json')
+    rit=r2.json()
+    detail_dic['India_list']=rit['statewise']
+    r=requests.get('https://corona.lmao.ninja/v2/countries')
+    detail_dic['World_list']=r.json()
+    w_total.update(requests.get('https://disease.sh/v3/covid-19/all?yesterday=true&twoDaysAgo=true&allowNull=true').json())
+    for i in detail_dic['World_list']:
+        if i['country']=='India':
+            i_total.update(i)
+            break
+th_details=Thread(target=getDetails)
+th_details.start()
+
 
 
 
@@ -39,12 +49,13 @@ app.config["DEBUG"] = True
 
 
 @app.route('/', methods=['GET'])
-def home(India_list= India_list,World_list= World_list,i_total=i_total,w_total=w_total):
-    return render_template("India_Covid.html", ilist=India_list,wlist=World_list,i_total=i_total,w_total=w_total)
+def home():
+    th_details.join()
+    return render_template("India_Covid.html", ilist=detail_dic['India_list'],wlist=detail_dic['World_list'],i_total=i_total,w_total=w_total)
 
 @app.route('/news', methods=['GET'])
 def news_sec():
-    th.join()
+    th_news.join()
     return render_template("news.html", newsList= news_dic['news_list'])
     # jsonify(news.get_news())
 
@@ -65,16 +76,18 @@ def analysis_sec(dataw=analysis.get_world_daily_data(),datai=analysis.get_india_
     return render_template("Analysis.html",dataw=dataw,datai=datai,datawd=datawd,dataid=dataid,zip=zip)
 
 @app.route('/api/world', methods=['GET'])
-def api_world(World_list=World_list):
-    return jsonify(World_list)
+def api_world():
+    th_details.join()
+    return jsonify(detail_dic['World_list'])
 
 @app.route('/api/india', methods=['GET'])
-def api_india(India_list=India_list):
-    return jsonify(India_list)
+def api_india():
+    th_details.join()
+    return jsonify(detail_dic['India_list'])
 
 @app.route('/api/news', methods=['GET'])
 def api_news():
-    th.join()
+    th_news.join()
     return jsonify(news_dic['news_list'])
 
 if __name__ == "__main__":
